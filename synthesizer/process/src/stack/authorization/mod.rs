7f2984db3abf84e06aeb4dc54013440a36e43c2e
Luke Newman <5315645+lukenewman@users.noreply.github.com>
2024-06-04 14:39:53 -0400

    clippy

--- synthesizer/process/src/stack/authorization/mod.rs
+++ synthesizer/process/src/stack/authorization/mod.rs
@@ -64,7 +64,7 @@ impl<N: Network> TryFrom<(Vec<Request<N>>, Vec<Transition<N>>)> for Authorizatio
         // Move the first request to the back in order to match the transitions.
         let mut requests_deque = VecDeque::from(requests);
         if let Some(first_request) = requests_deque.pop_front() {
-          requests_deque.push_back(first_request);
+            requests_deque.push_back(first_request);
         }
         // Ensure the requests and transitions are in order.
         for (index, (request, transition)) in requests_deque.iter().zip_eq(&transitions).enumerate() {

c512ae75008f912aa71d8e4bfd411e39e4699fbe
Luke Newman <5315645+lukenewman@users.noreply.github.com>
2024-06-04 21:41:33 -0400

    back in business

--- synthesizer/process/src/stack/authorization/mod.rs
+++ synthesizer/process/src/stack/authorization/mod.rs
@@ -62,12 +62,12 @@ impl<N: Network> TryFrom<(Vec<Request<N>>, Vec<Transition<N>>)> for Authorizatio
             transitions.len()
         );
         // Move the first request to the back in order to match the transitions.
-        // let mut requests_deque = VecDeque::from(requests.clone());
-        // if let Some(first_request) = requests_deque.pop_front() {
-        //     requests_deque.push_back(first_request);
-        // }
+        let mut requests_deque = VecDeque::from(requests.clone());
+        if let Some(first_request) = requests_deque.pop_front() {
+            requests_deque.push_back(first_request);
+        }
         // Ensure the requests and transitions are in order.
-        for (index, (request, transition)) in requests.iter().zip_eq(&transitions).enumerate() {
+        for (index, (request, transition)) in requests_deque.iter().zip_eq(&transitions).enumerate() {
             // Ensure the request and transition correspond to one another.
             ensure_request_and_transition_matches(index, request, transition)?;
         }
